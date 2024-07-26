5ebe32f869e45526185311bd5e076d68bf9ebceb
DiegolMarques <thediegomarques@gmail.com>
2024-07-26 10:47:29 -0400

    login fix

--- frontend/app/(auth)/index.tsx
+++ frontend/app/(auth)/index.tsx
@@ -19,7 +19,7 @@ export default function SigninScreen() {
                   password,
               }),
           });
-
+          
           const data = await response.json();
 
           if (data.success) {
